d86536eee72465ac2ee7b7c4e2a7a582ccce1460
Yurij Chekalyuk <yura_chekalyuk@mail.ru>
2021-06-04 22:51:43 +0300

    Initial Commit

---
+++ ColorChangerSwiftUI/ColorChangerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorChangerSwiftUIApp.swift
+//  ColorChangerSwiftUI
+//
+//  Created by SummeR on 04.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorChangerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
